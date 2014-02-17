e79312e030d7b6f1e84719726fc08c9e45f1de6a
jphillpot <jphillpot@hmc.edu>
2014-02-16 20:50:38 -0800

    Figured out how to use a custom viewController. Now we use the CycleOfViolenceViewController.

---
+++ ParseStarterProject/ParseStarterProject/CycleOfViolenceViewController.h
@@ -0,0 +1,13 @@
+//
+//  CycleOfViolenceViewController.h
+//  ParseStarterProject
+//
+//  Created by John Phillpot on 2/16/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CycleOfViolenceViewController : UIViewController
+
+@end
---
+++ ParseStarterProject/ParseStarterProject/CycleOfViolenceViewController.m
@@ -0,0 +1,38 @@
+//
+//  CycleOfViolenceViewController.m
+//  ParseStarterProject
+//
+//  Created by John Phillpot on 2/16/14.
+//
+//
+
+#import "CycleOfViolenceViewController.h"
+
+@interface CycleOfViolenceViewController ()
+
+@end
+
+@implementation CycleOfViolenceViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
