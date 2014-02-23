08d91bc5a2f818424c2b2b6be822e1d270adf145
Reyna Hulett <rhulett@g.hmc.edu>
2014-02-23 11:12:48 -0800

    Added a new CycleOfViolence project, linked to Parse
    Doesn’t do anything yet. I made the prefix COV, I hope that’s okay.

---
+++ CycleOfViolence/CycleOfViolence/COVAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  COVAppDelegate.h
+//  CycleOfViolence
+//
+//  Created by Laptop 16 on 2/23/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface COVAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
