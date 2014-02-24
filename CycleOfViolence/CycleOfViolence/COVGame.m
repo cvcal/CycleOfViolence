d12b6e0fe8d4eab1e6fcb4f024d766f84c75e1e4
jphillpot <jphillpot@hmc.edu>
2014-02-23 21:55:21 -0800

    Can now create games with custom names, and no players.

--- CycleOfViolence/CycleOfViolence/COVGame.m
+++ CycleOfViolence/CycleOfViolence/COVGame.m
@@ -11,6 +11,13 @@
 
 @implementation COVGame
 
+// These declarations tell the (pre)compiler to generate the
+// getters and setters necessary to make a PFObject work.
+@dynamic cycle;
+@dynamic name;
+@dynamic numberOfPlayers;
+@dynamic playersRemaining;
+
 // Required to make a proper subclass of PFObject.
 + (NSString *)parseClassName {
     return @"COVGame";
@@ -21,10 +28,10 @@
     self = [super init];
     if(self)
     {
-        //self.cycle = [[NSMutableArray alloc] init];
-        //self.numberOfPlayers = 0;
-        //self.playersRemaining = 0;
-        //[self setName:gameName];
+        self.cycle = [[NSMutableArray alloc] init];
+        self.numberOfPlayers = 0;
+        self.playersRemaining = 0;
+        self.name = gameName;
     }
     
     //TODO: Add the player who created the game!
@@ -46,46 +53,45 @@
     ++self.playersRemaining;
 }
 
-- (void) setNumberOfPlayers:(u_int32_t)numberOfPlayers
-{
-    self.numberOfPlayers = numberOfPlayers;
-}
-
-- (void) setPlayersRemaining:(u_int32_t)playersRemaining
-{
-    self.playersRemaining = playersRemaining;
-}
-
-- (void) setName:(NSString *)Name
-{
-    //self.name = Name;
-    //self.name = [[NSString alloc] init];
-}
-
-- (void)setCycle:(NSMutableArray *) cycle
-{
-    self.cycle = cycle;
-}
-
-- (NSString *)name
-{
-    return self.name;
-}
-
-- (u_int32_t)numberOfPlayers
-{
-    return self.numberOfPlayers;
-}
-
-- (u_int32_t)playersRemaining
-{
-    return self.playersRemaining;
-}
-
-- (NSMutableArray *)cycle
-{
-    return self.cycle;
-}
+//- (void) setNumberOfPlayers:(u_int32_t)numberOfPlayers
+//{
+//    self.numberOfPlayers = numberOfPlayers;
+//}
+//
+//- (void) setPlayersRemaining:(u_int32_t)playersRemaining
+//{
+//    self.playersRemaining = playersRemaining;
+//}
+//
+//- (void) setName:(NSString *)Name
+//{
+//    self.name = Name;
+//}
+//
+//- (void)setCycle:(NSMutableArray *) cycle
+//{
+//    self.cycle = cycle;
+//}
+//
+//- (NSString *)name
+//{
+//    return self.name;
+//}
+//
+//- (u_int32_t)numberOfPlayers
+//{
+//    return self.numberOfPlayers;
+//}
+//
+//- (u_int32_t)playersRemaining
+//{
+//    return self.playersRemaining;
+//}
+//
+//- (NSMutableArray *)cycle
+//{
+//    return self.cycle;
+//}
 
 
 @end
