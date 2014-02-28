b61e490a0f553a81d7617959978360c9b9b99176
jphillpot <jphillpot@hmc.edu>
2014-02-27 22:41:36 -0800

    Added COVJoinGameViewController. It now displays a list of all current games.

---
+++ CycleOfViolence/CycleOfViolence/COVJoinGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  COVJoinGameViewController.h
+//  ParseStarterProject
+//
+//  Created by James Yu on 12/29/11.
+//  Adapted by John Phillpot and Reyna Hulett 2/27/2014.
+//
+
+#import <Parse/Parse.h>
+
+@interface COVJoinGameViewController : PFQueryTableViewController
+
+@end
