e03b993e332ab66a38e1c7c620a77ed9d42945af
ChloeCalvarin <chloe.calvarin@gmail.com>
2014-03-06 22:57:10 -0800

    New Game Main view controller I added a new view controller for the “In Game : it has started!” case to parallel the “In Game : it will hopefully start soon!” case. The two controllers display a temporary message to help identify where you are.

---
+++ CycleOfViolence/CycleOfViolence/COVGameMainViewController.h
@@ -0,0 +1,17 @@
+//
+//  COVGameMainViewController.h
+//  CycleOfViolence
+//
+//  Created by Chloé Calvarin on 3/6/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface COVGameMainViewController : UIViewController
+
+- (id)initWithCoder:(NSCoder *)aDecoder;
+- (void)viewDidLoad;
+- (void)didReceiveMemoryWarning;
+
+@end
---
+++ CycleOfViolence/CycleOfViolence/COVGameMainViewController.m
@@ -0,0 +1,38 @@
+//
+//  COVGameMainViewController.m
+//  CycleOfViolence
+//
+//  Created by Chloé Calvarin on 3/6/14.
+//
+//
+
+#import "COVGameMainViewController.h"
+
+@interface COVGameMainViewController ()
+
+@end
+
+@implementation COVGameMainViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
