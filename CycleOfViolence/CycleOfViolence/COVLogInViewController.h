c658d54ff6669ed1cf026d7ef8277926beb4b510
John Phillpot <jphillpot@hmc.edu>
2014-04-17 21:43:58 -0700

    Added Subclass of PFLogInViewController.
    Including a placeholder image to test the new ViewController.

---
+++ CycleOfViolence/CycleOfViolence/COVLogInViewController.h
@@ -0,0 +1,15 @@
+//
+//  COVLogInViewController.h
+//  CycleOfViolence
+//
+//  Created by Laptop 16 on 4/17/14.
+//
+//
+
+#import <Parse/Parse.h>
+
+@interface COVLogInViewController : PFLogInViewController
+
+@property (nonatomic, strong) UIImageView *fieldsBackground;
+
+@end
