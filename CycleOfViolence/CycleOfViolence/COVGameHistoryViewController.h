967bfe3cee2a92fad5f62c92544e57c55177016d
John Phillpot <jphillpot@hmc.edu>
2014-04-25 00:05:36 -0700

    Restored Game History VCs.

---
+++ CycleOfViolence/CycleOfViolence/COVGameHistoryViewController.h
@@ -0,0 +1,13 @@
+//
+//  COVGameHistoryViewController.h
+//  CycleOfViolence
+//
+//  Created by Laptop 16 on 4/25/14.
+//
+//
+
+#import "COVQueryTableViewController.h"
+
+@interface COVGameHistoryViewController : COVQueryTableViewController
+
+@end
