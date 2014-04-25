e44b0557f33293fb29db7356afde580f190bb625
John Phillpot <jphillpot@hmc.edu>
2014-04-24 23:05:04 -0700

    Added COVGameHistory VC.

---
+++ CycleOfViolence/COVGameHistory.h
@@ -0,0 +1,13 @@
+//
+//  COVGameHistory.h
+//  CycleOfViolence
+//
+//  Created by John Phillpot on 4/24/14.
+//
+//
+
+#import "COVQueryTableViewController.h"
+
+@interface COVGameHistory : COVQueryTableViewController
+
+@end
---
+++ CycleOfViolence/COVGameHistory.m
@@ -0,0 +1,13 @@
+//
+//  COVGameHistory.m
+//  CycleOfViolence
+//
+//  Created by John Phillpot on 4/24/14.
+//
+//
+
+#import "COVGameHistory.h"
+
+@implementation COVGameHistory
+
+@end
