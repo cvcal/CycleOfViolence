eb5b65551b01f4fe305774537f16e18dd3bc11c2
John Phillpot <jphillpot@hmc.edu>
2014-04-26 12:47:31 -0700

    Changed navBars to display first name only.

--- CycleOfViolence/CycleOfViolence/COVQueryTableViewController.m
+++ CycleOfViolence/CycleOfViolence/COVQueryTableViewController.m
@@ -34,7 +34,8 @@
     // Add the name to the navigation bar. We need to do this in viewDidAppear
     // so that we update the user when the modal login view is dismissed.
     PFUser *currUser = [PFUser currentUser];
-    NSString *title = [NSString stringWithFormat:@"Welcome, %@!", currUser[@"fullName"]];
+    NSString *userFirstName = [[currUser[@"fullName"] componentsSeparatedByString:@" "] objectAtIndex:0];
+    NSString *title = [NSString stringWithFormat:@"Welcome, %@!", userFirstName];
     [self.navigationItem setTitle:title];
 }
 
--- CycleOfViolence/CycleOfViolence/COVViewController.m
+++ CycleOfViolence/CycleOfViolence/COVViewController.m
@@ -35,7 +35,9 @@
     // Add the name to the navigation bar. We need to do this in viewDidAppear
     // so that we update the user when the modal login view is dismissed.
     PFUser *currUser = [PFUser currentUser];
-    NSString *title = [NSString stringWithFormat:@"Welcome, %@!", currUser[@"fullName"]];
+    // Break the user's name into words and select the first.
+    NSString *userFirstName = [[currUser[@"fullName"] componentsSeparatedByString:@" "] objectAtIndex:0];
+    NSString *title = [NSString stringWithFormat:@"Welcome, %@!", userFirstName];
     [self.navigationItem setTitle:title];
 }
 
