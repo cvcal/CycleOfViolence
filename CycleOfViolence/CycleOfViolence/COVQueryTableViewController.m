89b28f3e3c32f89d0c2166c894368010380c5f72
ChloeCalvarin <chloe.calvarin@gmail.com>
2014-04-27 10:38:30 -0700

    Navigation bar coloring.

--- CycleOfViolence/CycleOfViolence/COVQueryTableViewController.m
+++ CycleOfViolence/CycleOfViolence/COVQueryTableViewController.m
@@ -24,6 +24,14 @@
                                    [UIImage imageNamed:
                                     @"blood_splatter_transparency_by_sagacious-d3h1yw6.png"]];
     [self.view addSubview:backgroundView];
+
+    // Change appearance of navigation bar.
+    self.navigationController.navigationBar.tintColor = [UIColor blackColor];
+    self.navigationController.navigationBar.barTintColor =
+                [UIColor colorWithRed:96/255.0f green:77/255.0f blue:96/255.0f alpha:0.5f];
+    self.navigationController.navigationBar.translucent = YES;
+    [self.navigationController.navigationBar setTitleTextAttributes:
+                @{NSForegroundColorAttributeName : [UIColor lightTextColor]}];
     
     // Send image behind the text/storyboard stuff.
     [self.view sendSubviewToBack:backgroundView];
--- CycleOfViolence/CycleOfViolence/COVViewController.m
+++ CycleOfViolence/CycleOfViolence/COVViewController.m
@@ -18,14 +18,23 @@
 {
     [super viewDidLoad];
     
-    //add background blood splatter, with a black background
+    // Add background blood splatter, with a black background.
     self.view.backgroundColor = [UIColor blackColor];
     UIImageView *backgroundView = [[UIImageView alloc] initWithImage:
                                    [UIImage imageNamed:
                                     @"blood_splatter_transparency_by_sagacious-d3h1yw6.png"]];
     [self.view addSubview:backgroundView];
     
-    // send image behind the text/storyboard stuff.
+    // Change appearance of navigation bar.
+    self.navigationController.navigationBar.tintColor = [UIColor blackColor];
+    self.navigationController.navigationBar.barTintColor =
+                [UIColor colorWithRed:96/255.0f green:77/255.0f blue:96/255.0f alpha:0.5f];
+    self.navigationController.navigationBar.translucent = YES;
+    [self.navigationController.navigationBar setTitleTextAttributes:
+                @{NSForegroundColorAttributeName : [UIColor lightTextColor]}];
+
+    
+    // Send image behind the text/storyboard stuff.
     [self.view sendSubviewToBack:backgroundView];
 }
 
