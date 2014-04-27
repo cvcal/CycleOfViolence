78f20c8691518c305488d6d66a36b5ff7d239c15
ChloeCalvarin <chloe.calvarin@gmail.com>
2014-04-27 11:09:05 -0700

    Fixed the save. It wasn’t checking for success, so it transitioned to home screen when the user wasn’t saved properly.

--- CycleOfViolence/CycleOfViolence/COVCreateGameViewController.m
+++ CycleOfViolence/CycleOfViolence/COVCreateGameViewController.m
@@ -61,87 +61,98 @@
                                        delegate:nil
                               cancelButtonTitle:@"ok"
                               otherButtonTitles:nil] show];
-        } else {
-            NSLog(@"Checking if the name is a duplicate.");
+            // re-enable create button during operation
+            [self.createButton setEnabled:YES];
             
-            // Verify that the game name is not taken by an unstarted game.
-            PFQuery *query = [PFQuery queryWithClassName:@"COVGame"];
-            [query whereKey:@"name" equalTo:self.name.text];
-            [query whereKey:@"state" equalTo:@(waitingToStart)];
-            NSInteger numberGamesWithSameName = [query countObjects];
-            NSLog(@"Queried parse for game name.");
-            
-            if (numberGamesWithSameName != 0) {
-                // Display an alert.
-                [[[UIAlertView alloc] initWithTitle:@"Duplicate Game Name"
-                                            message:@"That name is already in use!"
+            return;
+        }
+        
+        NSLog(@"Checking if the name is a duplicate.");
+        
+        // Verify that the game name is not taken by an unstarted game.
+        PFQuery *query = [PFQuery queryWithClassName:@"COVGame"];
+        [query whereKey:@"name" equalTo:self.name.text];
+        [query whereKey:@"state" equalTo:@(waitingToStart)];
+        NSInteger numberGamesWithSameName = [query countObjects];
+        NSLog(@"Queried parse for game name.");
+        
+        if (numberGamesWithSameName != 0) {
+            // Display an alert.
+            [[[UIAlertView alloc] initWithTitle:@"Duplicate Game Name"
+                                        message:@"That name is already in use!"
+                                       delegate:nil
+                              cancelButtonTitle:@"ok"
+                              otherButtonTitles:nil] show];
+            // re-enable create button during operation
+            [self.createButton setEnabled:YES];
+            return;
+        }
+        
+        // Get date from date field so we can check if the game is valid.
+        NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+        [dateFormatter setDateFormat:@"MM-dd-yyy HH:mm"];
+        [dateFormatter setTimeZone:[NSTimeZone localTimeZone]];
+        
+        NSDate *startDateFromInput = [dateFormatter dateFromString:self.startDateField.text];
+        
+        if (startDateFromInput == nil) {
+            // Date format failed
+            [[[UIAlertView alloc]
+              initWithTitle:@"Date Format Incorrect"
+              message:@"Please format the start time as MM-dd-yyy HH:mm"
+              delegate:nil
+              cancelButtonTitle:@"ok"
+              otherButtonTitles:nil] show];
+            // re-enable create button during operation
+            [self.createButton setEnabled:YES];
+            return;
+        }
+        
+        COVGame *newGame = [COVGame alloc];
+        NSLog(@"Allocated COVGame");
+        // Use the name from the UITextField.
+        newGame = [newGame initWithName:self.name.text];
+        
+        NSString *defaultRules = @"Basic Rules: When the game starts, you will receive the name of one other player, your target. Your goal is to assassinate them by meeting the kill criterion, below. Meanwhile, someone else will be trying to assassinate you; if they succeed, you will report it and be removed from the game. You win by being the last one alive.\n\n";
+        newGame.rules = [defaultRules stringByAppendingString:self.rules.text];
+        newGame.startTime = startDateFromInput;
+        newGame.actualStartTime = startDateFromInput;
+        NSLog(@"Initialized COVGame");
+        
+        // Save the game.
+        [newGame saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+            if (succeeded) {
+                NSLog(@"Sucessfully saved in background.");
+                PFUser *creator = [PFUser currentUser];
+                // We have to do this here because the COVGame's
+                // objectId is null until it has been saved.
+                newGame[@"uniqueId"] = newGame.objectId;
+                [newGame addPlayer:creator];
+                [newGame saveInBackground];
+                
+                [creator saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+                    // Now can proceed to home screen.
+                    if (succeeded){
+                        [self performSegueWithIdentifier:@"toHomeScreenFromCreate"
+                                                  sender:self];
+                    }
+                }];
+                
+            } else {
+                NSLog(@"Failed to save in background.");
+                [[[UIAlertView alloc] initWithTitle:@"Cloud save failed"
+                                            message:@"Please try again some other time."
                                            delegate:nil
                                   cancelButtonTitle:@"ok"
                                   otherButtonTitles:nil] show];
-            } else {
-                // Get date from date field so we can check if the game is valid.
-                NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
-                [dateFormatter setDateFormat:@"MM-dd-yyy HH:mm"];
-                [dateFormatter setTimeZone:[NSTimeZone localTimeZone]];
-                
-                NSDate *startDateFromInput = [dateFormatter dateFromString:self.startDateField.text];
-                
-                if (startDateFromInput != nil) {
-                    COVGame *newGame = [COVGame alloc];
-                    NSLog(@"Allocated COVGame");
-                    // Use the name from the UITextField.
-                    newGame = [newGame initWithName:self.name.text];
-                    
-                    NSString *defaultRules = @"Basic Rules: When the game starts, you will receive the name of one other player, your target. Your goal is to assassinate them by meeting the kill criterion, below. Meanwhile, someone else will be trying to assassinate you; if they succeed, you will report it and be removed from the game. You win by being the last one alive.\n\n";
-                    newGame.rules = [defaultRules stringByAppendingString:self.rules.text];
-                    newGame.startTime = startDateFromInput;
-                    newGame.actualStartTime = startDateFromInput;
-                    NSLog(@"Initialized COVGame");
-                    
-                    // Save the game.
-                    [newGame saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
-                        if (succeeded) {
-                            NSLog(@"Sucessfully saved in background.");
-                            PFUser *creator = [PFUser currentUser];
-                            // We have to do this here because the COVGame's
-                            // objectId is null until it has been saved.
-                            newGame[@"uniqueId"] = newGame.objectId;
-                            [newGame addPlayer:creator];
-                            [newGame saveInBackground];
-                            
-                            [creator saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
-                                // Now can proceed to home screen.
-                                [self performSegueWithIdentifier:@"toHomeScreenFromCreate"
-                                                          sender:self];
-                            }];
-                            
-                        } else {
-                            NSLog(@"Failed to save in background.");
-                            [[[UIAlertView alloc] initWithTitle:@"Cloud save failed"
-                                                        message:@"Please try again some other time."
-                                                       delegate:nil
-                                              cancelButtonTitle:@"ok"
-                                              otherButtonTitles:nil] show];
-                        }
-                    }];
-                } else {
-                    // Date format failed
-                    [[[UIAlertView alloc]
-                                  initWithTitle:@"Date Format Incorrect"
-                                        message:@"Please format the start time as MM-dd-yyy HH:mm"
-                                        delegate:nil
-                                cancelButtonTitle:@"ok"
-                                otherButtonTitles:nil] show];
-                }
+                // re-enable create button during operation
+                [self.createButton setEnabled:YES];
             }
-        }
+        }];
     } else {
         // This shouldn't happen.
         NSLog(@"Called buttonTapped via some other means");
     }
-    
-    // re-enable create button during operation
-    [self.createButton setEnabled:YES];
 }
 
 - (BOOL)textFieldShouldReturn:(UITextField *)textField
